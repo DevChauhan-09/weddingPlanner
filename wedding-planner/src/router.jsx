06a2c9c23224939a40d1d562798b323b5d914761
Apurva Salphale <salphaleapurvaa@gmail.com>
2025-07-25 13:25:11 +0530

    router

---
+++ wedding-planner/src/router.jsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Vendors from "./pages/Vendors";
+import Venue from "./pages/Venue";
+import E_Invites from "./pages/E_Invites";
+import Real_Wedding from "./pages/Real_Wedding";
+import Login from "./auth/Login";
+import SignUp from "./auth/SignUp";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> },
+      { path: "vendors", element: <Vendors /> },
+      { path: "venue", element: <Venue /> },
+      { path: "e-invites", element: <E_Invites /> },
+      { path: "real-wedding", element: <Real_Wedding /> },
+      { path: "login", element: < Login />},
+      { path: "signUp", element: < SignUp />},
+    ],
+  },
+]);
+
+export default router;
